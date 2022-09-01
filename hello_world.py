692020ab229f4718431a67f8ccfc6eec971d6160
Gianni Castillo <66094112+giannicastillo@users.noreply.github.com>
2022-08-31 21:20:56 -0400

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,33 @@
+# print("Hello World")
+
+#######################
+
+# name = "Gianni"
+
+# print("Hello" , name)
+#########################
+
+#NinjaBonus 
+# name = "Gianni"
+
+# print("Hello {}".format(name))
+
+##################
+
+# num = 2
+
+# print("Hello" , str(num))
+
+############################
+
+# food_one = "watermelon"
+# food_two = "mangos"
+
+# print("I love {} and {}".format(food_one , food_two))
+
+##################################
+
+# food_one = "watermelon"
+# food_two = "mangos"
+
+# print(f"I love {food_one} and {food_two}")
